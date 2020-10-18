3805791faf123c61b0e206908068d5475c6cab08
Koki Ryu <liukoki@gmail.com>
2020-10-18 16:20:09 +0900

    add fetch

---
+++ core/fetch.v
@@ -0,0 +1,29 @@
+module fetch(clk, state, pc, instr_out);
+    input clk;
+
+    // FETCH = 0
+    // DECODE = 1
+    // EXEC = 2
+    // MEM = 3
+    // WRITE = 4
+    // TODO: Make the constants global
+    input [2:0] state;
+    input [9:0] pc;
+    output reg [31:0] instr_out;
+    reg [7:0] instr_mem [1023:0];
+
+    integer i;
+    //TODO insert valid instructions later
+    initial begin
+        for(i = 0;i<=1023;i=i+1)
+            instr_mem[i] = 0;
+        for(i = 0;i<=255;i=i+1)
+            instr_mem[i * 4] = i;
+    end
+
+    always @(posedge clk) begin
+        if (state == 0)
+            instr_out <= {instr_mem[pc + 3], instr_mem[pc + 2], instr_mem[pc + 1], instr_mem[pc]};
+    end
+
+endmodule
---
+++ core/tests/fetch_tb.v
@@ -0,0 +1,28 @@
+`timescale 1ns/1ps
+module fetch_tb();
+    reg clk;
+    reg [2:0] state;
+    reg [9:0] pc;
+    wire [31:0] instr_out;
+
+    fetch fetch_instance(clk, state, pc, instr_out);
+
+    always #10 clk = ~clk;
+    always #20 pc = pc + 4;
+
+    initial begin
+        clk = 0;
+        pc = 0;
+        //fetch
+        state = 0;
+        //finish fetching
+        #500 state = 1;
+
+        #1000 $finish();
+    end
+
+    initial begin
+        $dumpfile("fetch_test.vcd");
+        $dumpvars(1, fetch_instance);
+    end
+endmodule
