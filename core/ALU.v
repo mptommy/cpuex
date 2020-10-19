dad195c34af28177aa8eaa2c0e178b3530df98ff
Koki Ryu <liukoki@gmail.com>
2020-10-19 17:05:17 +0900

    32bit ALU

--- core/ALU.v
+++ core/ALU.v
@@ -1,10 +1,14 @@
 module ALU (ALUctl, A, B, ALUOut, Zero);
     input [3:0] ALUctl;
-    input [63:0] A, B;
-    output reg[63:0] ALUOut;
+    input [31:0] A, B;
+    output reg[31:0] ALUOut;
     output Zero;
     assign Zero = (ALUOut == 0);
-    //TODO: Use 32bit alu
+
+    initial begin
+        ALUOut = 0;
+    end
+
     always @ (ALUctl, A, B) begin
         case (ALUctl)
             0: ALUOut <= A & B;
@@ -12,9 +16,9 @@ module ALU (ALUctl, A, B, ALUOut, Zero);
             2: ALUOut <= A + B;
             6: ALUOut <= A - B;
             7: ALUOut <= A < B ? 1 : 0;
-            //8: GE
-            //9: Choose A
-            //10: Choose B
+            8: ALUOut <= A >= B ? 1 : 0;
+            9: ALUOut <= A;
+            10: ALUOut <= B;
             12: ALUOut <= ~ (A | B); //nor
             default: ALUOut <= 0;
         endcase
--- core/tests/ALU_tb.v
+++ core/tests/ALU_tb.v
@@ -1,33 +1,59 @@
 `timescale 1ns/1ps
 module ALU_tb();
-    reg [63:0] A, B;
-    wire [63:0] ALUOut;
+    reg [31:0] A, B;
+    wire [31:0] ALUOut;
     reg [3:0] ALUctl;
     wire Zero;
 
     ALU ALU_instance(ALUctl, A, B, ALUOut, Zero);
-    initial begin
-        A = 5;
-        B = 12;
-    end
 
     initial begin
         ALUctl = 0;
-        #10 $write("%b & %b = %b\n", A, B, ALUOut);
-        #20 ALUctl = 1;
-        #30 $write("%b | %b = %b\n", A, B, ALUOut);
-        #40 ALUctl = 2;
-        #50 $write("%b + %b = %b\n", A, B, ALUOut);
-        #60 ALUctl = 6;
-        #70 $write("%b - %b = %b\n", A, B, ALUOut);
-        #80 ALUctl = 7;
-        #90 $write("%b < %b = %b\n", A, B, ALUOut);
-        #100 ALUctl = 12;
-        #110 $write("%b nor %b = %b\n", A, B, ALUOut);
+        A = 0;
+        B = 0;
+        //and
+        #10 ALUctl = 0;
+        A = 3;
+        B = 13;
+        //or
+        #10 ALUctl = 1;
+        A = 5;
+        B = 12;
+        //+
+        #10 ALUctl = 2;
+        A = 3;
+        B = - 7;
+        //Nop
+        #10 ALUctl = 3;
+        A = 312;
+        B = 23;
+        //-
+        #10 ALUctl = 6;
+        A = - 31;
+        B = 512;
+        // <
+        #10 ALUctl = 7;
+        A = 11;
+        B = 1;
+        // >=
+        #10 ALUctl = 8;
+        A = 42;
+        B = 555;
+        // A
+        #10 ALUctl = 9;
+        A = 22;
+        B = 3142;
+        //B
+        #10 ALUctl = 10;
+        A = 5;
+        B = 2;
+        #10 ALUctl = 12;
+        A = 4;
+        B = 1;
     end
 
     initial begin
-        #1000 $finish();
+        #200 $finish();
     end
 
     initial begin
