254748e76e98cdd29a65f4bfb2c8141ceab8f7b6
Koki Ryu <liukoki@gmail.com>
2020-10-26 14:24:44 +0900

    use .mem files

--- core/fetch.v
+++ core/fetch.v
@@ -20,43 +20,7 @@ module fetch(clk, state, pc, instr_out);
     //TODO insert valid instructions later
     initial begin
         instr_out = 0;
-        for(i = 0;i<=1023;i=i+1)
-            instr_mem[i] = 0;
-        for(i = 0;i<=255;i=i+1)
-            instr_mem[i * 4] = i;
-        //main:
-        //    addi    x1,x0,4
-        instr_mem[0] = 8'h93;
-        instr_mem[1] = 8'h00;
-        instr_mem[2] = 8'h40;
-        instr_mem[3] = 8'h00;
-        //    addi    x2,x0,3
-        instr_mem[4] = 8'h13;
-        instr_mem[5] = 8'h01;
-        instr_mem[6] = 8'h30;
-        instr_mem[7] = 8'h00;
-        //    blt     x2,x1,truth
-        instr_mem[8] = 8'h63;
-        instr_mem[9] = 8'h44;
-        instr_mem[10] = 8'h11;
-        instr_mem[11] = 8'h00;
-        //    jal     x0,loop
-        instr_mem[12] = 8'h6f;
-        instr_mem[13] = 8'h00;
-        instr_mem[14] = 8'h80;
-        instr_mem[15] = 8'h00;
-        //truth:
-        //    add     x1,x1,x1
-        instr_mem[16] = 8'hb3;
-        instr_mem[17] = 8'h80;
-        instr_mem[18] = 8'h10;
-        instr_mem[19] = 8'h00;
-        //loop:
-        //    jal     x0,loop
-        instr_mem[20] = 8'h6f;
-        instr_mem[21] = 8'h00;
-        instr_mem[22] = 8'h00;
-        instr_mem[23] = 8'h00;
+        $readmemh("./core/tests/memory.mem", instr_mem);
     end
 
     always @(posedge clk) begin
